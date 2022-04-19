a43a77788aed1c2c4402b65d6ee7ebb8a5e16c55
Nazme Al Nahian <nazme.nahian@northsouth.edu>
2022-04-19 20:28:42 +0600

    Update tests.py

--- saveLifeApp/tests.py
+++ saveLifeApp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+"""
+    Testing if user input password matches with confirm password data.
+"""
+
+
+class TestRegister(TestCase):
+    def test_register(self):
+        assert password == confirmPassword
+
+    def test_organRequest(self):
+        assert num.length == '11'
